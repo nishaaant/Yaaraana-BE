66028d8fd664b09f1c2f7e1b949b49b4712efa21
nishaaant <nishaaant1608@gmail.com>
2025-01-01 21:10:33 +0530

    multiple route handlers

--- src/App.js
+++ src/App.js
@@ -2,27 +2,26 @@ const express = require("express");
 
 const app = express();
 
+app.use(
+    "/test" ,
+    (req,res,next) => {
+        console.log("1");
+    // res.send("posted it!!1");
+    next();
+},
+(req,res,next) => {
+    console.log("2");
+    // res.send("posted it!!2");
+    next();
+},
+(req,res,next) => {
+    console.log("3");
+    res.send("posted it!!3");
+    next();
+}
+);
 
 
-app.get("/test/:userID/:name/:password" , (req,res) => {
-    console.log(req.params)
-    res.send("got it!!");
-});
-app.post("/test" , (req,res) => {
-    res.send("posted it!!");
-});
-app.delete("/test" , (req,res) => {
-    res.send("Deleted it!!");
-});
-
-
-app.use("/college" , (req,res) => {
-    res.send("Gautam Buddha University");
-});
-
-app.use("/", (req,res) => {
-    res.send("hello");
-});
 app.listen(9999 , () => {
     console.log("Server ran succesfully...");
 });
