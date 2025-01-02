20e9ee69e436326bd0b438e98e9d32e7ebfb631c
nishaaant <nishaaant1608@gmail.com>
2025-01-02 18:24:25 +0530

    Get APIS

--- src/App.js
+++ src/App.js
@@ -7,6 +7,40 @@ const app = express();
 
 app.use(express.json());
 
+//get data by user id
+app.get("/user", async (req,res) => {
+    const userID = req.body._id;
+    try{
+        const user = await User.findById(userID)
+        if(!user){
+            res.status(404).send("User not Found")
+        }else{
+            res.send(user)
+        }
+    }
+    catch(err) {
+        res.status(400).send("Error Faced")
+    }
+})
+
+//get all the users
+app.get("/feed", async (req,res) => {
+    try{
+        const users = await User.find({})
+        if(users.length == 0){
+            res.status(404).send("No Data")
+        }else{
+            res.send(users)
+        }
+    }
+    catch(err) {
+        res.status(400).send("Error Faced")
+    }
+})
+
+
+
+//post user data
 app.post("/signup", async (req,res) => {
     const user = new User(req.body)
 
