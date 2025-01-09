4fe2756119d6dd78526036e81ecfb15e4d7ce374
nishaaant <nishaaant1608@gmail.com>
2025-01-09 16:31:18 +0530

    db url p2

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async() => {
     await(
         mongoose.connect(
-            "mongodb+srv://learnnode-username:learnmongodb1@learnnode-cluster.jpymb.mongodb.net/Yaaraana/retryWrites=true"
+            "mongodb+srv://learnnode-username:learnmongodb1@learnnode-cluster.jpymb.mongodb.net/Yaaraana/?retryWrites=true"
             )
     )
 }
