11c726ffa7658bef0d836e3377ba3086146f183c
nishaaant <nishaaant1608@gmail.com>
2025-01-12 17:56:52 +0530

    web 2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
     cors({
-      origin: "https://yaaraana-nish.vercel.app/",
+      origin: "https://yaaraana-nish.vercel.app",
       credentials: true,
     })
   );
